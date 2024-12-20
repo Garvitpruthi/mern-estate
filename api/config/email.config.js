2142ba0c0a6df0b29c7e70903c4b88cc14be3dab
Garvit Pruthi <garvitpruthi9817@gmail.com>
2024-12-20 21:00:23 +0530

    added keys to env

--- api/config/email.config.js
+++ api/config/email.config.js
@@ -1,11 +1,13 @@
 import nodemailer from 'nodemailer';
+import {config} from './server.config.js';
+
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: config.HOST,
   port: 587,
   secure: false, // true for port 465, false for other ports
   auth: {
-    user: "garvitpruthi476@gmail.com",
-    pass: "snip mtxe hpte nxhv",
+    user: config.user,
+    pass: config.pass,
   },
 });
--- api/config/server.config.js
+++ api/config/server.config.js
@@ -6,4 +6,7 @@ export const config = {
     MONGO : process.env.MONGO,
     JWT: process.env.JWT_SECRET,
     PORT: process.env.PORT || 3000,
+    HOST: process.env.HOST,
+    user: process.env.user,
+    pass: process.env.pass
 }
